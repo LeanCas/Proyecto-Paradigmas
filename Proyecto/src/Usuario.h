64fae1e676b6e755820b9ca9ce3e9a1e8b47a8fc
LeanCas <leandrocasavalle10@gmail.com>
2023-11-24 17:05:25 -0300

    creacion de la clase Usuario.h

---
+++ Proyecto/src/Usuario.h
@@ -0,0 +1,39 @@
+/*
+ * Usuario.h
+ *
+ *  Created on: 24 nov. 2023
+ *      Author: Alumno
+ */
+
+#ifndef USUARIO_H_
+#define USUARIO_H_
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Usuario {
+	int id;
+	static int autonumerico;
+	string nombre;
+	string apellido;
+	string paisOrigen;
+	string email;
+	string contrasenia;
+	vector<string> notificaciones;
+	vector<*Publicacion> likesRespuestas;
+	vector<*Publicacion> publicaciones;
+public:
+	Usuario();
+	~Usuario();
+	string getNombre();
+	void eliminarCuenta();
+	int contarRespuestasAceptadas();
+	void crearPregunta(int tipo=1, string img);
+	void crearRespuesta(Pregunta);
+	void aceptarSolucion(Respuesta);
+	void listarInformacion();
+};
+
+
+
+#endif /* USUARIO_H_ */
