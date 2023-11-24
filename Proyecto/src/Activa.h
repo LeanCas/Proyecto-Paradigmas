13d51d042588b4199868fd8960923e1e8789a44e
LeanCas <leandrocasavalle10@gmail.com>
2023-11-24 17:38:40 -0300

    creacion de clase activa.h

--- Proyecto/src/Activa.h
+++ Proyecto/src/Activa.h
@@ -7,9 +7,13 @@
 
 #ifndef ACTIVA_H_
 #define ACTIVA_H_
+#include "Estado.h"
 
-
-
-
+class Activa : public Estado {
+public:
+	Activa();
+	~Activa();
+	string getTipo();
+};
 
 #endif /* ACTIVA_H_ */
