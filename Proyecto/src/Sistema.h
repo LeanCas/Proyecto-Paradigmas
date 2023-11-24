a15487fb3021ab468a40efd897c74d94975d1f87
LeanCas <leandrocasavalle10@gmail.com>
2023-11-24 16:54:59 -0300

    creacion de sistema.h

---
+++ Proyecto/src/Sistema.h
@@ -0,0 +1,36 @@
+/*
+ * Sistema.h
+ *
+ *  Created on: 24 nov. 2023
+ *      Author: Alumno
+ */
+
+#ifndef SISTEMA_H_
+#define SISTEMA_H_
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Sistema {
+	string nombre;
+	vector<*Publicacion> publicaciones;
+	vector<*Usuario> usuarios;
+public:
+	Sistema();
+	~Sistema();
+	void crearUsuario(string, string, string, string);
+	void suspenderPublicacion(Publicacion);
+	void listarPublicaciones();
+	void eliminarPublicacion(int);
+	void darMG(int idUsuario, int idRespuesta);
+	Usuario getUsuario(int idUsuario);
+	Respuesta getRespuesta(int idRespuesta);
+	void rankearUsuarios();
+	void eliminarUsuario(int idUsuario);
+};
+
+
+
+
+
+#endif /* SISTEMA_H_ */
