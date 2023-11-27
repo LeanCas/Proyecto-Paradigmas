d2c7331edc775109b377183fe3d1a2b06fbf323c
LeanCas <leandrocasavalle10@gmail.com>
2023-11-27 15:35:59 -0300

    creacion de metodo Inactiva - getTipo

---
+++ Proyecto/src/Inactiva.cpp
@@ -0,0 +1,16 @@
+/*
+ * Inactiva.cpp
+ *
+ *  Created on: 27 nov. 2023
+ *      Author: Bruno Casavalle
+ */
+
+#include "Inactiva.h"
+
+string Inactiva :: getTipo(){
+	return 'Inactiva';
+}
+
+
+
+
