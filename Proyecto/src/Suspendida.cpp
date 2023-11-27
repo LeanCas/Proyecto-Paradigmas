eee03baedc8a461b6280d19b5c198e50f128013a
LeanCas <leandrocasavalle10@gmail.com>
2023-11-27 15:37:31 -0300

    creacion de metodo Suspendida - getTipo

---
+++ Proyecto/src/Suspendida.cpp
@@ -0,0 +1,15 @@
+/*
+ * Suspendida.cpp
+ *
+ *  Created on: 27 nov. 2023
+ *      Author: Bruno Casavalle
+ */
+
+#include "Suspendida.h"
+
+string Suspendida :: getTipo(){
+	return 'Suspendida';
+}
+
+
+
