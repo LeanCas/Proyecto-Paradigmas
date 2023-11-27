a99c936df66074ece53507466382724559cce7fb
LeanCas <leandrocasavalle10@gmail.com>
2023-11-27 15:36:51 -0300

    creacion de metodo Solucionada - getTipo

---
+++ Proyecto/src/Solucionada.cpp
@@ -0,0 +1,16 @@
+/*
+ * Solucionada.cpp
+ *
+ *  Created on: 27 nov. 2023
+ *      Author: Bruno Casavalle
+ */
+
+#include "Solucionada.h"
+
+string Solucionada :: getTipo(){
+	return 'Solucionada';
+}
+
+
+
+
