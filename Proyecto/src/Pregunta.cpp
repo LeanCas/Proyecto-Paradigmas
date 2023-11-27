8dca1d1d5d2d5054800176acc73983843c3bf9a6
Javier Granero <javiergranero89@gmail.com>
2023-11-27 02:25:11 -0300

    Se incluyeron los constructores predeterminado, sobrecargado y copia, además del destructor. También se incluye el override del método listarInformacion. Se incluyen los getters de Tipo e Id.

---
+++ Proyecto/src/Pregunta.cpp
@@ -0,0 +1,48 @@
+/*
+ * Pregunta.cpp
+ *
+ *  Created on: 26 nov. 2023
+ *      Author: javie
+ */
+
+#include "Pregunta.h"
+#include "Usuario.h"
+
+// Constructor predeterminado
+Pregunta::Pregunta(): Publicacion(), titulo(""), descripcion("") {
+	respuestas = vector<Respuesta*>();
+	// respuestas = {};
+}
+
+// Constructor sobrecargado
+Pregunta::Pregunta(int tipo, string imagen, Fecha fecha, Usuario* usuario, string titulo, string descripcion, vector<string> tags, vector<Respuesta*> respuestas): Publicacion(1, imagen, fecha, usuario), titulo(titulo), descripcion(descripcion), tags(tags) {}
+
+// Constructor copia
+Pregunta::Pregunta(const Pregunta& p): Publicacion(p), titulo(p.titulo), descripcion(p.descripcion), tags(p.tags) {
+	// Realiza una copia profunda del vector de respuestas
+	for (Respuesta* respuesta : p.respuestas) {
+		Respuesta* nuevaRespuesta = new Respuesta(*respuesta);
+		respuestas.push_back(nuevaRespuesta);
+	}
+}
+
+Pregunta::Pregunta(const Publicacion& publicacion) : Publicacion(publicacion) {}
+
+// Destructor
+Pregunta::~Pregunta() {
+	for (Respuesta* respuesta : respuestas) {
+		delete respuesta;
+	}
+}
+
+void Pregunta::listarInformacion() {
+	cout << "Título: " << this->titulo << endl;
+	cout << "Descripción: " << this->descripcion << endl;
+	cout << "Imagen: " << this->imagen << endl;
+	//cout << "Estado: " << this->estado;
+	cout << "Fecha: " << this->fecha << endl;
+	cout << "Etiquetas: " << endl;
+	for (string T : this->tags) {
+		cout << T << endl;
+	}
+}
--- Proyecto/src/Pregunta.h
+++ Proyecto/src/Pregunta.h
@@ -10,6 +10,8 @@
 #include <iostream>
 #include <vector>
 #include "Publicacion.h"
+#include "Respuesta.h"
+#include "Estado.h"
 
 using namespace std;
 
@@ -18,20 +20,24 @@ class Pregunta : public Publicacion {
 	string descripcion;
 	vector<string> tags;
 	vector<Respuesta*> respuestas;
+
 public:
 	Pregunta();
+	Pregunta(int tipo=1, string imagen="", Fecha fecha=Fecha(), Usuario* usuario=nullptr, string titulo="", string descripcion="", vector<string> tags={}, vector<Respuesta*> respuestas={});
+	Pregunta(const Pregunta& p);
+	// Constructor que acepta una Publicacion
+	Pregunta(const Publicacion& publicacion);
 	~Pregunta();
 	void agregarRespuesta(Respuesta);
-	void cambiarEstado(Estado);
+	//void cambiarEstado(Estado);
 	void rankearRespuesta();
-	void listarInformacion();
+	void listarInformacion() override;
 	string getTitulo();
 	void listarRespuestas();
+
 private:
 	void notificarUsuario(Respuesta);
 };
-
-
 
 
 #endif /* PREGUNTA_H_ */
