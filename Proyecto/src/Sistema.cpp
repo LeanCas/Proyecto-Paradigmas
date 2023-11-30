0231db882e72af831ac8f4e7e82d22f39a8f76dc
Javier Granero <javiergranero89@gmail.com>
2023-11-30 14:07:17 -0300

    Se verifican que cada método coincida con el diseño UML. Se corrigieron los métodos eliminarUsuario y eliminarPublicacion (ahora eliminan el elemento del vector asociado), ahora buscarPublicacion es getPublicacion. Se ordena el código. Verificaciones terminadas, falta comprobar su correcto funcionamiento

--- Proyecto/src/Sistema.cpp
+++ Proyecto/src/Sistema.cpp
@@ -8,7 +8,7 @@
 #include <algorithm>
 #include "Sistema.h"
 #include "Usuario.h"
-#include "Suspendida.h"
+#include "Pregunta.h"
 
 Sistema::Sistema(string nombre){
 	this->nombre = nombre;
@@ -17,131 +17,6 @@ Sistema::Sistema(string nombre){
 void Sistema :: crearUsuario(string nombre, string apellido, string paisOrigen, string email, string contrasenia){
 	Usuario* nuevoUsuario = new Usuario(nombre, apellido, paisOrigen, email, contrasenia);
 	this->usuarios.push_back(nuevoUsuario);
-}
-
-void Sistema::listarPublicaciones()
-{
-	for(Publicacion* publicacion : publicaciones)
-	{
-		if(publicacion->getTipo() == 1)
-		{
-			//Pregunta* pregunta = (Pregunta*)publicacion;
-			Pregunta* pregunta = dynamic_cast<Pregunta*>(publicacion);
-			cout<<endl << pregunta->getTitulo();
-			pregunta->listarRespuestas();
-		}
-	}
-}
-
-Publicacion* Sistema::buscarPublicacion(int id)
-{
-	Publicacion* Encontrado = nullptr;
-	vector<Publicacion*>::iterator it;
-	for(it = publicaciones.begin(); it != publicaciones.end(); ++it)
-	{
-		if((*it)->getId() == id)
-		{
-			Encontrado = (*it);
-		}
-	}
-	return Encontrado;
-}
-
-/*void Sistema :: suspenderPublicacion(Publicacion* publicacion){
-
-	Pregunta* pregunta = dynamic_cast<Pregunta*>(publicacion);
-
-	Fecha fechaActual;
-	fechaActual.setFechaActual();
-	Fecha fechaPublicacion;
-	fechaPublicacion = pregunta->getFecha();
-
-	if(fechaActual.getMes() - fechaPublicacion.getMes() >= 6){
-		Estado* nuevoEstado = new Suspendida();
-		pregunta->cambiarEstado(nuevoEstado);
-	}
-}*/
-
-void Sistema::eliminarPublicacion(int id)
-{
-	Publicacion* publicacionAEliminar = buscarPublicacion(id);
-	delete publicacionAEliminar; //Invoca al destructor de la clase derivada correspondiente
-}
-
-Usuario* Sistema::getUsuario(int id) {
-	Usuario* Encontrado;
-	vector<Usuario*>::iterator it;
-	for(it = usuarios.begin(); it!=usuarios.end(); ++it)
-	{
-		if((*it)->getId()==id)
-		{
-			Encontrado = (*it);
-			break;
-		}
-	}
-	return Encontrado;
-}
-
-Respuesta* Sistema::getRespuesta(int idRespuesta) {
-	Respuesta* Encontrado = dynamic_cast<Respuesta*>(buscarPublicacion(idRespuesta));
-	return Encontrado;
-}
-
-Pregunta* Sistema::getPregunta(int idPregunta){
-	Pregunta* Encontrado = dynamic_cast<Pregunta*>(buscarPublicacion(idPregunta));
-	return Encontrado;
-}
-
-void Sistema::eliminarUsuario(int idUsuario)
-{
-	Usuario* UsuarioEliminar = getUsuario(idUsuario);
-	UsuarioEliminar->eliminarCuenta();
-}
-
-Sistema::~Sistema()
-{
-	for (Publicacion* publicacion : publicaciones)
-	{
-		delete publicacion;
-	}
-	publicaciones.clear();
-
-    for (Usuario* usuario : usuarios)
-    {
-        delete usuario;
-    }
-    usuarios.clear();
-}
-
-void Sistema::darMG(int idUsuario, int idRespuesta) {
-    Respuesta* respuesta = this->getRespuesta(idRespuesta);
-    Usuario* usuario = this->getUsuario(idUsuario);
-
-    if (respuesta != nullptr && usuario != nullptr && !usuario->pertenece(idRespuesta)) {
-        respuesta->darMG();
-        usuario->agregarRespuestaLikeada(respuesta);
-    }
-}
-
-
-void Sistema::rankearUsuarios(){
-	    sort(usuarios.begin(), usuarios.end(), [](Usuario* u1,Usuario* u2) {
-	        return u1->contarRespuestasAceptadas() > u2->contarRespuestasAceptadas();
-	    });
-	    cout << "Usuarios rankeados por respuestas aceptadas:" <<endl;
-	    for (Usuario* usuario : usuarios) {
-	        cout << "ID: "<<usuario->getId()<<", Nombre: "<<usuario->getNombre()<<endl;
-	    }
-}
-
-
-
-
-
-//AGREGADO ULTIMO
-
-void Sistema :: agregarPublicacion(Publicacion* publicacion){
-	this->publicaciones.push_back(publicacion);
 }
 
 void Sistema::crearPregunta(string imagen, string titulo, string descripcion, vector<string> tags, int idUsuario) {
@@ -153,10 +28,132 @@ void Sistema::crearPregunta(string imagen, string titulo, string descripcion, ve
 }
 
 void Sistema :: crearRespuesta(int idPregunta,int idUsuario,string imagen,string contenido){
-	Pregunta* pregunta = this->getPregunta(idPregunta);
+	Pregunta* pregunta = getPregunta(idPregunta);
 	Usuario* usuario = getUsuario(idUsuario);
 	if(pregunta != nullptr){
 		Respuesta* nuevaRespuesta = usuario->crearRespuesta(pregunta, imagen, contenido);
 		publicaciones.push_back(nuevaRespuesta);
 	}
+}
+
+void Sistema::suspenderPublicacion(int idPublicacion) {
+	Pregunta* publicacion = dynamic_cast<Pregunta*>(getPublicacion(idPublicacion));
+	Fecha fechaActual;
+	fechaActual.setFechaActual();
+	Fecha fechaPublicacion;
+	fechaPublicacion = publicacion->getFecha();
+
+	if(fechaActual.getMes() - fechaPublicacion.getMes() >= 6){
+		publicacion->cambiarEstado(publicacion->estadoSuspendida);
+	}
+}
+
+void Sistema::listarPublicaciones() {
+	for(Publicacion* publicacion : publicaciones) {
+		if(publicacion->getTipo() == 1) {
+			Pregunta* pregunta = dynamic_cast<Pregunta*>(publicacion);
+			pregunta->listarInformacion();
+			pregunta->listarRespuestas();
+		}
+	}
+}
+
+void Sistema::eliminarPublicacion(int idPublicacion) {
+	Publicacion* publicacionAEliminar = getPublicacion(idPublicacion);
+	if(publicacionAEliminar != nullptr) {
+
+		vector<Publicacion*>::iterator it;
+		for(it = publicaciones.begin(); it!=publicaciones.end(); ++it) {
+			if((*it) == publicacionAEliminar) {
+				delete (*it);
+				publicaciones.erase(it);
+				break;
+			}
+		}
+	}
+}
+
+void Sistema::eliminarUsuario(int idUsuario) {
+    Usuario* usuarioAEliminar = getUsuario(idUsuario);
+
+    if (usuarioAEliminar != nullptr) {
+        vector<Usuario*>::iterator it;
+        for (it = usuarios.begin(); it != usuarios.end(); ++it) {
+            if ((*it) == usuarioAEliminar) {
+                usuarios.erase(it);
+                break;
+            }
+        }
+        usuarioAEliminar->eliminarCuenta();
+    }
+}
+
+
+void Sistema::darMG(int idUsuario, int idRespuesta) {
+    Respuesta* respuesta = this->getRespuesta(idRespuesta);
+    Usuario* usuario = this->getUsuario(idUsuario);
+
+    if (respuesta != nullptr && usuario != nullptr && !usuario->pertenece(idRespuesta)) {
+        respuesta->darMG();
+        usuario->agregarRespuestaLikeada(respuesta);
+    }
+}
+
+void Sistema::rankearUsuarios(){
+	    sort(usuarios.begin(), usuarios.end(), [](Usuario* u1,Usuario* u2) {
+	        return u1->contarRespuestasAceptadas() > u2->contarRespuestasAceptadas();
+	    });
+	    cout << "Usuarios rankeados por respuestas aceptadas:" <<endl;
+	    for (Usuario* usuario : usuarios) {
+	        cout << "ID: "<<usuario->getId()<<", Nombre: "<<usuario->getNombre()<<endl;
+	    }
+}
+
+// Getters
+
+Publicacion* Sistema::getPublicacion(int idPublicacion) {
+	Publicacion* encontrado = nullptr;
+	vector<Publicacion*>::iterator it;
+	for(it = publicaciones.begin(); it != publicaciones.end(); ++it) {
+		if((*it)->getId() == idPublicacion) {
+			encontrado = (*it);
+		}
+	}
+	return encontrado;
+}
+
+Usuario* Sistema::getUsuario(int id) {
+	Usuario* Encontrado;
+	vector<Usuario*>::iterator it;
+	for(it = usuarios.begin(); it!=usuarios.end(); ++it) {
+		if((*it)->getId()==id) {
+			Encontrado = (*it);
+			break;
+		}
+	}
+	return Encontrado;
+}
+
+Respuesta* Sistema::getRespuesta(int idRespuesta) {
+	Respuesta* Encontrado = dynamic_cast<Respuesta*>(getPublicacion(idRespuesta));
+	return Encontrado;
+}
+
+Pregunta* Sistema::getPregunta(int idPregunta){
+	Pregunta* Encontrado = dynamic_cast<Pregunta*>(getPublicacion(idPregunta));
+	return Encontrado;
+}
+
+// Destructor
+
+Sistema::~Sistema() {
+	for (Publicacion* publicacion : publicaciones) {
+		delete publicacion;
+	}
+	publicaciones.clear();
+
+    for (Usuario* usuario : usuarios) {
+        delete usuario;
+    }
+    usuarios.clear();
 }
--- Proyecto/src/Sistema.h
+++ Proyecto/src/Sistema.h
@@ -18,29 +18,31 @@ using namespace std;
 
 class Sistema {
 	string nombre;
-	vector<Publicacion*> publicaciones;
-	vector<Usuario*> usuarios;
+	vector<Publicacion*> publicaciones;	// Agregación
+	vector<Usuario*> usuarios;	// Debe ser composición, no un vector de punteros (?)
 
 public:
 	Sistema(string);
 	~Sistema();
-	void crearUsuario(string, string, string, string, string);
-	void suspenderPublicacion(Publicacion*);
-	void listarPublicaciones();
-	Publicacion* buscarPublicacion(int);
-	void eliminarPublicacion(int);
-	void darMG(int idUsuario, int idRespuesta);
-	Usuario* getUsuario(int idUsuario);
-	Respuesta* getRespuesta(int idRespuesta);
-	Pregunta* getPregunta(int idPregunta);
-	void rankearUsuarios();
-	void eliminarUsuario(int idUsuario);
+	void crearUsuario(string nombre, string apellido, string paisOrigen, string email, string contrasenia);		// OK - revisado
+	void crearPregunta(string imagen, string titulo, string descripcion, vector<string> tags, int idUsuario);		// OK - revisado
+	void crearRespuesta(int idPregunta,int idUsuario,string imagen,string contenido);		// OK - revisado
+	void suspenderPublicacion(int idPublicacion);		// OK - revisado
+	void listarPublicaciones();		// OK - revisado
+	void eliminarPublicacion(int);		// OK - revisado
+	void eliminarUsuario(int idUsuario);		// OK - revisado
+	void darMG(int idUsuario, int idRespuesta);		// OK - revisado
+	void rankearUsuarios();		// OK - revisado
+
+	// Getters
+
+	Usuario* getUsuario(int idUsuario);		// OK - revisado
+	Respuesta* getRespuesta(int idRespuesta);		// OK - revisado
+	Pregunta* getPregunta(int idPregunta);		// OK - revisado
+	Publicacion* getPublicacion(int idPublicacion);		// OK - revisado
 
 
-	void agregarPublicacion(Publicacion*);
 
-	void crearPregunta(string imagen, string titulo, string descripcion, vector<string> tags, int idUsuario);
-	void crearRespuesta(int idPregunta,int idUsuario,string imagen,string contenido);
 };
 
 #endif /* SISTEMA_H_ */
