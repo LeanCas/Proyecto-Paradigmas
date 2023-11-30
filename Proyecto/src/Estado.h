cd21b4b36fd977595f93405c8a7b19066d3a3e0b
Javier Granero <javiergranero89@gmail.com>
2023-11-30 00:17:17 -0300

    Se declara el método para controlar la adición de respuestas en las preguntas, heredada por cada subclase

--- Proyecto/src/Estado.h
+++ Proyecto/src/Estado.h
@@ -25,7 +25,6 @@ public:
 	Estado();
 	virtual ~Estado();
 	virtual string getTipo();
-	virtual Estado* clonar() const = 0; 	// Método virtual puro para clonar el estado (se usa en los constructores copia de pregunta)
 	virtual void agregarRespuesta(Respuesta* respuesta, Pregunta* pregunta) = 0;	// Este método es el que cambia según el estado del contexto (Pregunta)
 };
 
